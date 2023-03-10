89fca03687f923b6ce249ce29187c2df5577b78c
rayyks <rayydna14@gmail.com>
2023-03-10 23:44:10 +0700

    adding up components file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 import { Routes, Route } from "react-router-dom";
-
+// Components Import
+import MovieProvider from "./Movie";
+import Panel from "./components/Panel";
+import Container from "./components/Container";
+import Details from "./components/Details";
+// CSS import
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MovieProvider>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Panel />
+                <Container />
+              </>
+            }
+          />
+          <Route path="/movie">
+            <Route path=":movieId" element={<Details />} />
+          </Route>
+        </Routes>
+      </MovieProvider>
+    </div>
+  );
 }
 
 export default App;
