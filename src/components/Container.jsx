fbf3855b051ff2608f1971ee808dfecce3588962
rayyks <rayydna14@gmail.com>
2023-03-11 14:33:25 +0700

    adding container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Filter from "./Filter";
+import Search from "./Search";
+import Header from "./Header";
+import MovieList from "./MovieList";
 
 const Container = () => {
-  return <div>Container</div>;
+  return (
+    <div className="container">
+      <Search />
+      <Filter />
+      <Header />
+      <MovieList />
+    </div>
+  );
 };
 
 export default Container;
